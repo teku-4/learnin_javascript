03b834817906a638cd1549463a316876a227bb2e
✨leta <letakasahun2@gmail.com>
2025-03-17 13:52:12 -0700

    map_and_set

---
+++ mat_and_set.js
@@ -0,0 +1,26 @@
+// // --------------------javascript_-----set and map-------------------------------
+// const countries=["Ethiopia","France","Austeralia"]
+// let countrySet=new Set(countries)
+// console.log(countrySet)
+// console.log(typeof countrySet)
+// const languages = ['English', 'Finnish','English','French','Spanish', 'English', 'French']
+// const languageSet=new Set(languages)
+//   console.log(languageSet)
+//   languageSet.forEach(lang=>console.log(lang))
+//   console.log(languageSet.size)
+//   languageSet.add("Amharic")
+//   languageSet.add("Chainese")
+//   console.log(languageSet)
+//   languageSet.delete("Finnish")
+// console.log(languageSet)
+// set intersection and difference of set achieved usig filter----------------
+// let numbers1=[12,13,14,15,56]
+// let numbers2=[12,34,56,78,56,26]
+// let a = new Set(numbers1)
+// let b = new Set(numbers2)
+// const c=numbers1.filter((num) => !b.has(num))
+// const C= new Set(c)
+// console.log(C)
+
+
+// ---------------------------------------------------------------
